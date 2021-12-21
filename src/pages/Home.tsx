e08b018042183dd837ceff14eb12755ac838daba
Valdir Silva <v.souza.da.silva@avanade.com>
2021-12-21 18:54:39 -0300

    feat: implements challenge 01 features

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,48 @@ import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
+import { Task, TasksList } from '../components/TasksList';
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    if(!newTaskTitle)
+      return
+    
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldTasks => [...oldTasks, newTask])
   }
-
+  
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
-  }
+    if(!id)
+      return
 
+    const updatedTasks = tasks.map(task => ({...task}))
+
+    const taskFiltered = updatedTasks.find(item => item.id === id)
+
+    if(!taskFiltered)
+      return
+
+    taskFiltered.done = !taskFiltered.done
+
+    setTasks(updatedTasks)
+    
+  }
+  
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    if(!id)
+      return
+
+    setTasks(oldTasks => oldTasks.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
